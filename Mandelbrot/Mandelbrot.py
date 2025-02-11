334a04b6b0ff7af8ab07fbd358a16eea6e76fd52
Max <96468663+maks0661@users.noreply.github.com>
2025-02-11 19:15:21 +0700

    Add files via upload

---
+++ Mandelbrot/Mandelbrot.py
@@ -0,0 +1,47 @@
+# Генерация фрактального изображение Мандельброта - один из самых красивых и известных математических объектов
+# Фракталы обладают свойством свмоподобия, то есть их структура повторяется при маштабировании
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def mandelbrot(c, max_iter):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < max_iter:
+        z = z * z + c
+        n += 1
+    return n
+
+
+def generate_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter):
+    # массив для хранения результатов
+    mandelbrot_set = np.zeros((height, width))
+
+    for x in range(width):
+        for y in range(height):
+            # преобразуем координаты пикселя в комплексное число
+            real = x_min + (x / width) * (x_max - x_min)
+            imag = y_min + (y / height) * (y_max - y_min)
+            c = complex(real, imag)
+
+            # кол-во итераций для данного комплексного числа
+            mandelbrot_set[y, x] = mandelbrot(c, max_iter)
+
+    return mandelbrot_set
+
+
+# параметры для генерации изображения
+width, height = 800, 600
+x_min, x_max = -2.5, 1.5
+y_min, y_max = -1.5, 1.5
+max_iter = 100
+
+# генерируем множество Мандельброта
+mandelbrot_image = generate_mandelbrot(width, height, x_min, x_max, y_min, y_max, max_iter)
+
+# юз матлотлиб для отображения изображения
+plt.imshow(mandelbrot_image, cmap='hot', extent=(x_min, x_max, y_min, y_max))
+plt.colorbar()
+plt.title("Множество Мандельброта")
+plt.show()
